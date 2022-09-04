3d2c1eca34801c6f3c8489c0cb660de4c080c778
AJunpei <junpeiazegami@gmail.com>
2022-09-04 23:28:26 +0900

    ApexChartを導入

--- pages/Menu/graph/index.vue
+++ pages/Menu/graph/index.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
-    <h1>vue-chartjsで棒グラフ</h1>
-    <ItemSumLineChart />
+    <apexchart
+      width="500"
+      type="line"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
   </div>
 </template>
 
 <script>
-import ItemSumLineChart from '@/components/chart/ItemSumLineChart'
+import Vue from 'vue'
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+
+Vue.component('apexchart', VueApexCharts)
 
 export default {
-  components: {
-    ItemSumLineChart
+  data () {
+    return {
+      chartOptions: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [
+        {
+          name: 'グラフ名',
+          data: [30, 40, 35, 50, 49, 60, 70, 91]
+        }
+      ]
+    }
   }
 }
 </script>
